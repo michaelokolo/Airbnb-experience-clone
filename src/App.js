f752e760efc17653f5614a8f244dba7932c8d27a
Michael Okolo <michaelokolo62@yahoo.com>
2023-03-16 08:27:43 -0400

    completed airbnb project

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import './App.css';
 import Hero from './Hero'
 import Navbar from './Navbar'
 import Card from './Card'
-import Joke from './Joke'
-import jokesData from './jokesData';
 import carddata from './data'
 
 function App() {
   const cardElements = carddata.map(data => {
-    return <Card 
-            img={data.coverImg}
-            rating={data.stats.rating}
-            reviewCount={data.stats.reviewCount}
-            location = {data.location}
-            title = {data.title}
-            price={data.price} 
-          /> 
+    return <Card
+              key = {data.id}
+              {...data}
+            /> 
   })
 
   
   return (
     <div className='container'>
-      {/*<Navbar />*/}
-      {/*<Hero />*/}
-      {cardElements}
+      <Navbar />
+      <Hero />
+      <section className='cards-list'>
+        {cardElements}
+      </section>
       
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -2,24 +2,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/fontawesome-free-solid"
 
 function Card(props){
-    
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
 return(
         <section className="card">
             <div className="card-container">
                 <div className="card-image">
-                    <img src={props.coverImg}/>
-                    <p className="status">SOLD OUT</p>
+                    <img src={`./${props.coverImg}`}/>
+                    {badgeText && <p className="status">{badgeText}</p>}
                 </div>
                 <div className="card-text">
-                    <p><FontAwesomeIcon icon={faStar} className="star"/> {props.rating}<span className="gray">({props.reviewCount}) {props.location}</span></p>
+                    <p><FontAwesomeIcon icon={faStar} className="star"/> {props.stats.rating}<span className="gray">({props.stats.reviewCount}) {props.location}</span></p>
                     <p>{props.title}</p>
                     <p><span className="bold">From ${props.price}</span> / person</p>
                 </div>
             </div>
-          
-
-            
-
         </section>
     )
 }
